6f715151e7d252c5ddbd312e7be133ff47bc25da
Leigh Douglas <leighcdouglas1@gmail.com>
2016-12-17 18:46:37 -0500

    boot broadcast sets alarm

--- app/src/main/java/nyc/c4q/leighdouglas/memehunt/BootBroadCastReceiver.java
+++ app/src/main/java/nyc/c4q/leighdouglas/memehunt/BootBroadCastReceiver.java
@@ -16,22 +16,20 @@ public class BootBroadCastReceiver extends WakefulBroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // Construct an intent that will execute the AlarmReceiver
-//        Intent i = new Intent(context, MemeAlarmReceiver.class);
-//
-//        // Create a PendingIntent to be triggered when the alarm goes off
-//        final PendingIntent pendingIntent = PendingIntent.getBroadcast(context, MemeAlarmReceiver.REQUEST_CODE, i, PendingIntent.FLAG_UPDATE_CURRENT);
-//
-//        long firstMillis = System.currentTimeMillis(); // alarm is set right away
-//
-//        AlarmManager alarm = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-//
-//        // First parameter is the type: ELAPSED_REALTIME, ELAPSED_REALTIME_WAKEUP, RTC_WAKEUP
-//        // Interval can be INTERVAL_FIFTEEN_MINUTES, INTERVAL_HALF_HOUR, INTERVAL_HOUR, INTERVAL_DAY
-//        alarm.setInexactRepeating(AlarmManager.RTC_WAKEUP, firstMillis, AlarmManager.INTERVAL_FIFTEEN_MINUTES / 15, pendingIntent);
-//        startWakefulService(context, i);
+        //Construct an intent that will execute the AlarmReceiver
+        Intent i = new Intent(context, MemeAlarmReceiver.class);
 
-        Intent i = new Intent(context, NotificationService.class);
+        // Create a PendingIntent to be triggered when the alarm goes off
+        final PendingIntent pendingIntent = PendingIntent.getBroadcast(context, MemeAlarmReceiver.REQUEST_CODE, i, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        long firstMillis = System.currentTimeMillis(); // alarm is set right away
+
+        AlarmManager alarm = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        // First parameter is the type: ELAPSED_REALTIME, ELAPSED_REALTIME_WAKEUP, RTC_WAKEUP
+        // Interval can be INTERVAL_FIFTEEN_MINUTES, INTERVAL_HALF_HOUR, INTERVAL_HOUR, INTERVAL_DAY
+        alarm.setInexactRepeating(AlarmManager.RTC_WAKEUP, firstMillis, AlarmManager.INTERVAL_FIFTEEN_MINUTES / 15, pendingIntent);
+
         startWakefulService(context, i);
 
     }
